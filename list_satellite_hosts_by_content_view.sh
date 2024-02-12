e63152e73c7f2918b3b0473f7dadc1056768dd7d
cshabazian <chip@shabazian.com>
2024-02-12 11:07:04 -0800

    Update list_satellite_hosts_by_content_view.sh

--- list_satellite_hosts_by_content_view.sh
+++ list_satellite_hosts_by_content_view.sh
@@ -1,2 +1,2 @@
-
+#!/bin/bash
 for i in $(hammer content-view list | tail -n +4 | sed '$d' | awk '{print $1}') ; do hammer content-view info --id $i --fields "Id","Name","Content host count" | tr '\n' ',' ; echo -e "" ; done
