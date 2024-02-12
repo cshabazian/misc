dbd4b2fe57dc26b431eb0b946f84df05d3f6bf50
cshabazian <chip@shabazian.com>
2024-02-12 11:04:41 -0800

    Create list_satellite_hosts_by_content_view.sh
    This will list all content views on a Satellite Server and show how many hosts are registered to each content view

---
+++ list_satellite_hosts_by_content_view.sh
@@ -0,0 +1,2 @@
+
+for i in $(hammer content-view list | tail -n +4 | sed '$d' | awk '{print $1}') ; do hammer content-view info --id $i --fields "Id","Name","Content host count" | tr '\n' ',' ; echo -e "" ; done
